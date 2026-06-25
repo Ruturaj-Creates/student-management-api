ba8f90556a88795cc3372f30b5f874f6ffb7f218
Ruturaj-Creates <ruturajdesai40@gmail.com>
2026-06-25 20:20:10 +0530

    SCRUM-8: Implement create student endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status
+from schemas import StudentCreate,StudentResponse
+from data import students
 
 app=FastAPI()
 
@@ -8,3 +10,17 @@ def root():
     return{
         "message":"Student API Running"
     }
+
+@app.post("/students",response_model=StudentResponse,status_code=status.HTTP_201_CREATED)
+def new_student(student:StudentCreate):
+
+    new_id=len(students)+1
+
+    new_student=StudentResponse(
+        id=new_id,
+        name=student.name,
+        age=student.age,
+        email=student.email 
+          )
+    students.append(new_student)
+    return new_student
