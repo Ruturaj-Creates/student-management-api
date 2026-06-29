c9cfd6bc2d28f300b84d8c6fa7f7beadcc7fc58e
Ruturaj-Creates <ruturajdesai40@gmail.com>
2026-06-29 19:57:18 +0530

    chore: add proper data in data.py

--- data.py
+++ data.py
@@ -1,1 +1,28 @@
-students = []
+from schemas import StudentResponse
+
+students = [
+    StudentResponse(
+        id=1,
+        name="Rahul Sharma",
+        age=20,
+        email="rahul.sharma@example.com"
+    ),
+    StudentResponse(
+        id=2,
+        name="Priya Verma",
+        age=21,
+        email="priya.verma@example.com"
+    ),
+    StudentResponse(
+        id=3,
+        name="Aman Patel",
+        age=19,
+        email="aman.patel@example.com"
+    ),
+    StudentResponse(
+        id=4,
+        name="Sneha Kulkarni",
+        age=22,
+        email="sneha.kulkarni@example.com"
+    )
+]
